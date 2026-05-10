7505005e7e2d32908e57f2c9c0bb5e746cc2626c
Rafael Souza <sdrafa@hotmail.com>
2026-05-10 15:43:22 -0300

    Alteração de hostname e criação das Vlans

---
+++ backend/switch_config.py
@@ -0,0 +1,50 @@
+from netmiko import ConnectHandler
+
+device = {
+    "device_type": "cisco_ios",
+    "host": "192.168.1.100",
+    "username": "admin",
+    "password": "admin123",
+}
+
+
+commands = [
+
+    "hostname SWITCH_AUTOMATIZADO",
+
+    "interface GigabitEthernet1.10",
+    "encapsulation dot1Q 10",
+    "ip address 192.168.10.1 255.255.255.0",
+    "description VLAN_DADOS",
+    "no shutdown",
+
+    "interface GigabitEthernet1.20",
+    "encapsulation dot1Q 20",
+    "ip address 192.168.20.1 255.255.255.0",
+    "description VLAN_VOZ",
+    "no shutdown",
+
+    "interface GigabitEthernet1.50",
+    "encapsulation dot1Q 50",
+    "ip address 192.168.50.1 255.255.255.0",
+    "description VLAN_SEGURANCA",
+    "no shutdown",
+]
+
+try:
+    connection = ConnectHandler(**device)
+
+    print("Conectado com sucesso!\n")
+
+    output = connection.send_config_set(commands)
+
+    print(output)
+
+    connection.save_config()
+
+    print("\nConfiguração salva com sucesso!")
+
+    connection.disconnect()
+
+except Exception as e:
+    print(f"Erro: {e}")
