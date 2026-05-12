b3347323910993ce24898140ad01064e953701d4
sdrafa369 <sdrafa.floripa@gmail.com>
2026-05-12 10:09:15 -0300

    Alteração no README e prints

--- Automação Configuração de Switch/backend/switch_config.py
+++ Automação Configuração de Switch/backend/switch_config.py
@@ -45,17 +45,12 @@ def configurar_switch(
         f"ip address {vlan3_ip} 255.255.255.0",
         f"description {vlan3_nome}",
         "no shutdown",
-        
     ]
 
     try:
 
         connection = ConnectHandler(**device)
-        
-        print("=" * 50)
-        print("APLICANDO CONFIGURAÇÕES...")
-        print("=" * 50)
-        
+
         output = connection.send_config_set(commands)
 
         print(output)
@@ -68,11 +63,7 @@ def configurar_switch(
 
         connection.disconnect()
 
-        return (
-        "Configuração aplicada com sucesso!\n"
-        "Hostname alterado.\n"
-        "Configuração salva na NVRAM."
-        )
+        return "Configuração aplicada com sucesso!"
 
     except Exception as e:
         return f"Erro: {e}"
